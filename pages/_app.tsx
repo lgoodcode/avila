43e6588905b77018add2880b2b4f35f72c3d2de4
Lawrence Good <lawrence.good55@gmail.com>
2022-05-28 10:10:42 -0700

    Moved viewport meta tag
    Not to use the Viewport `meta` tag in `_document.js` but, instead to use
    it within the `_app.tsx` file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,19 @@ import Hydrate from '../components/Hydrate'
 import theme from '../theme'
 import 'aos/dist/aos.css'
 import '../styles/global.css'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
 			<Hydrate>
+				<Head>
+					{/* Prevent mobile from zooming in with pinch - not to be used in _document.js */}
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+					/>
+				</Head>
 				<Component {...pageProps} />
 			</Hydrate>
 		</ChakraProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,6 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					{/* Prevent mobile from zooming in with pinch */}
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-					/>
 					<link
 						href="https://fonts.googleapis.com/css?family=Roboto|Inter&display=optional"
 						rel="stylesheet"
